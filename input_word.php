aedf1be9f8ec2901766c9c61a364a54842cbce93
WonHo Bark <wonho.park83@gmail.com>
2017-05-04 17:39:06 +0900

    remove comment

--- input_word.php
+++ input_word.php
@@ -1,10 +1,10 @@
 <?php
 @require('conn.php');
 global $mysql;
-/*
+
 echo "<form action="/insert_word.php" method="post">
 추천 단어<input type='text' name-'word'><br>
 등록<input type='submit' value="Submit">
 </form>";
-*/
+
 ?>
