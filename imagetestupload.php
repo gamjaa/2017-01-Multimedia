5aa28a08d64f7fbfc9e5794c271b0d99e0d2ddef
정경원 <potato93@daum.net>
2017-05-04 13:19:53 +0900

    오타 수정

--- imagetestupload.php
+++ imagetestupload.php
@@ -5,7 +5,7 @@ $uploadDir = '/var/www/html/upload/';
 $uploadFile = $uploadDir . basename($_FILES['userfile']['name']);
 
 echo '<pre>';
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadFile)) {
     echo "파일이 유효하고, 성공적으로 업로드 되었습니다.\n";
     $query = "update imagetest set image_data=".basename($_FILES['userfile']['name'])." where image_id=0;";
     $mysqli->query($query);
