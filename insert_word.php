d1a155da633c9cc0f7dd15d472c8a98fe12b40e3
WonHo Bark <wonho.park83@gmail.com>
2017-05-04 18:06:52 +0900

    test

--- insert_word.php
+++ insert_word.php
@@ -1,10 +1,10 @@
 <?php
 @require('conn.php');
 global $mysqli;
-/*
+
 $query = "INSERT INTO word (word_data)
           VALUES (\'".$_POST['word']."\')";
-*/
+
 echo $_POST['word'];
 echo "<br>";
 echo $query;
