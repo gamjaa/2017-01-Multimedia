18bbb8e8002e2b7264832dcaa8f1a915e0420c2d
정경원 <potato93@daum.net>
2017-05-04 13:26:57 +0900

    이미지 업로드 쿼리문 확인 가능하게 수정

--- imagetestupload.php
+++ imagetestupload.php
@@ -9,6 +9,7 @@ if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadFile)) {
     echo "파일이 유효하고, 성공적으로 업로드 되었습니다.\n";
     $query = "update imagetest set image_data='".basename($_FILES['userfile']['name'])."' where image_id=0;";
     $mysqli->query($query);
+    echo $query;
 } else {
     print "파일 업로드 공격의 가능성이 있습니다!\n";
 }
