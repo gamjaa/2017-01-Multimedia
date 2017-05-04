199513de061017e46bdcee46e29c34282b73a4cc
WonHo Bark <wonho.park83@gmail.com>
2017-05-04 17:56:04 +0900

    change code

--- insert_word.php
+++ insert_word.php
@@ -2,10 +2,9 @@
 @require('conn.php');
 global $mysqli;
 
-/*
 $query = "INSERT INTO word (word_data)
-          VALUES ($_POST['word'])";
+          VALUES (".$_POST['word'].")";
 $mysqli->query($query);
-*/
+
 echo $_POST['word'];
 ?>
