2ee325e562cf70f061308ddc3d8f182b727bc427
WonHo Bark <wonho.park83@gmail.com>
2017-05-04 17:40:57 +0900

    change return

--- input_word.php
+++ input_word.php
@@ -2,9 +2,8 @@
 @require('conn.php');
 global $mysql;
 
-echo "<form action="/insert_word.php" method="post">
-추천 단어<input type='text' name-'word'><br>
-등록<input type='submit' value="Submit">
+echo "<form action='/insert_word.php' method='post'>
+추천 단어<input type='text' name:'word'><br>
+등록<input type='submit' value='Submit'>
 </form>";
-
 ?>
