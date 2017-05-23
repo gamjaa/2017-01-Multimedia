c9756f4ff0feaef5116d9758698287ee0c65d49a
정경원 <potato93@daum.net>
2017-05-23 09:58:32 +0900

    POST 처리 소스 작성

--- draw.php
+++ draw.php
@@ -10,87 +10,134 @@
   - word 테이블에서 랜덤으로 단어 불러와서 출력
   - 그림 그리기 후 저장, room 생성(gamePlay 데이터 생성, room_order = 2, room_play_id 입력)
 */
-
 @require('conn.php');
 global $mysqli;
 
-// 진행 중인 게임 확인
-$query = "select * from gameRoom where room_order != 0";
-$result = $mysqli->query($query);
-$gameRoomCount = 0;
-$gameRoom = array();
-while($data = mysqli_fetch_array($result)){
-  $gameRoom[$gameRoomCount]['room_id'] = $data['room_id'];
-  $gameRoom[$gameRoomCount]['room_order'] = $data['room_order'];
-  $gameRoom[$gameRoomCount]['room_word_id'] = $data['room_word_id'];
-  $gameRoom[$gameRoomCount]['room_play_id'] = $data['room_play_id'];
-  $gameRoomCount++;
-}
+if(isset($_POST['submit'])) {
+  // 게임 플레이 정보 입력
+  $play_room_id = $_POST['play_room_id'];
+  $play_order = $_POST['play_order'];
 
-// room_order가 홀수인 게임 확인
-if($gameRoomCount != 0) {
-  for($i=0; $i<$gameRoomCount; $i++) {
-    if($gameRoom[$i]['room_order'] % 2 == 1) {
-      break;
-    }
-    else if($i == $gameRoomCount - 1) {
-      $gameRoomCount = 0;
-      break;
-    }
+  // 게임방, 차례에 맞는지 확인
+  $query = "select * from gameRoom where room_id={$play_room_id} and room_order={$play_order};";
+  $result = $mysqli->query($query);
+  $check = 0;
+  while($data = mysqli_fetch_array($result)){
+    $check++;
   }
-}
+  if($check != 0) {
+    // 이미지 업로드
+    $uploadDir = '/var/www/html/upload/';
+    $uploadFile = date("U").substr($_FILES['userfile']['tmp_name'], 8).".".pathinfo($_FILES['userfile']['name'])['extension'];
+    $uploadPath = $uploadDir . $uploadFile;
 
-// *** 진행 중인 게임 있을 경우, 그림 그리기 ***
-if($gameRoomCount != 0) {
-  /*
-  $gameRoom[$i]['room_id']
-  $gameRoom[$i]['room_order']
-  $gameRoom[$i]['room_word_id']
-  $gameRoom[$i]['room_play_id']
-  */
-  if($gameRoom[$i]['room_order'] == 1) {
-    $query = "select * from word where word_id='{$gameRoom[$i]['room_word_id']}'";
-    $result = $mysqli->query($query);
-    $data = mysqli_fetch_array($result);
-    $word = $data['word_data'];
+    if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadPath)) {
+      // 이미지명 DB 저장
+      $query = "insert into gamePlayImage(image_data) values (image_data='{$uploadFile}');";
+      $mysqli->query($query);
+
+      // 게임 플레이 정보 저장
+      $query = "insert into gamePlay(play_room_id, play_room_order, play_data) values({$play_room_id}, {$play_order}, {$mysqli->insert_id})";
+      $mysqli->query($query);
+
+      // 게임방 정보 변경
+      $query = "update gameRoom set room_order={$play_order+1}, room_play_id={$mysqli->insert_id} where room_id={$play_room_id};";
+      $mysqli->query($query);
+
+      echo "<script language='javascript'>
+              alert('성공!');
+            </script>"
+    }
+    else {
+      echo "<script language='javascript'>
+              alert('이미지 업로드 실패!');
+            </script>"
+    }
   }
   else {
-    $query = "select * from gamePlay where play_id='{$gameRoom[$i]['room_play_id']}'";
-    $result = $mysqli->query($query);
-    $data = mysqli_fetch_array($result);
-    $word = $data['play_data'];
+    echo "<script language='javascript'>
+            alert('이미 진행된 순서입니다.');
+          </script>"
   }
-
-  echo "제시어: {$word}\n";
-  echo "<form enctype='multipart/form-data' action='drawSubmit.php' method='POST'>
-      <!-- MAX_FILE_SIZE는 file 입력 필드보다 먼저 나와야 합니다 -->
-      <input type='hidden' name='MAX_FILE_SIZE' value='3000000' />
-      <input type='hidden' name='play_room_id' value='{$gameRoom[$i]['room_id']}' />
-      <!-- input의 name은 $_FILES 배열의 name을 결정합니다 -->
-      이 파일을 전송합니다: <input name='userfile' type='file' />
-      <input type='submit' value='파일 전송' />
-      </form>";
 }
-// *** 진행 중인 게임 없을 경우, gameRoom 생성 후 새로고침 ***
 else {
-  // word 테이블에서 단어 불러오기 후 랜덤으로 한 개 선정
-  $query = "select * from word";
+  // 진행 중인 게임 확인
+  $query = "select * from gameRoom where room_order != 0";
   $result = $mysqli->query($query);
-  $i = 0;
-  $wordset = array();
+  $gameRoomCount = 0;
+  $gameRoom = array();
   while($data = mysqli_fetch_array($result)){
-    $wordset[$i]['word_id'] = $data['word_id'];
-    $wordset[$i]['word_data'] = $data['word_data'];
-    $i++;
+    $gameRoom[$gameRoomCount]['room_id'] = $data['room_id'];
+    $gameRoom[$gameRoomCount]['room_order'] = $data['room_order'];
+    $gameRoom[$gameRoomCount]['room_word_id'] = $data['room_word_id'];
+    $gameRoom[$gameRoomCount]['room_play_id'] = $data['room_play_id'];
+    $gameRoomCount++;
   }
-  $answer = mt_rand(0, $i-1);
 
-  // gameRoom 생성 후 새로고침
-  $query = "insert into gameRoom (room_word_id) values ('{$wordset[$answer]['word_id']}')";
-  $mysqli->query($query);
-  echo "<script language='javascript'>
-          document.location.reload();
-        </script>";
+  // room_order가 홀수인 게임 확인
+  // TODO: 본인이 참여하지 않은 게임 확인
+  if($gameRoomCount != 0) {
+    for($i=0; $i<$gameRoomCount; $i++) {
+      if($gameRoom[$i]['room_order'] % 2 == 1) {
+        break;
+      }
+      else if($i == $gameRoomCount - 1) {
+        $gameRoomCount = 0;
+        break;
+      }
+    }
+  }
+
+  // *** 진행 중인 게임 있을 경우, 그림 그리기 ***
+  if($gameRoomCount != 0) {
+    /*
+    $gameRoom[$i]['room_id']
+    $gameRoom[$i]['room_order']
+    $gameRoom[$i]['room_word_id']
+    $gameRoom[$i]['room_play_id']
+    */
+    if($gameRoom[$i]['room_order'] == 1) {
+      $query = "select * from word where word_id='{$gameRoom[$i]['room_word_id']}'";
+      $result = $mysqli->query($query);
+      $data = mysqli_fetch_array($result);
+      $word = $data['word_data'];
+    }
+    else {
+      $query = "select * from gamePlay where play_id='{$gameRoom[$i]['room_play_id']}'";
+      $result = $mysqli->query($query);
+      $data = mysqli_fetch_array($result);
+      $word = $data['play_data'];
+    }
+
+    echo "제시어: {$word}\n";
+    // TODO: POST 값 수정을 통한 공격을 막기 위해 서버로부터 계산된 검증 데이터 전송 필요
+    echo "<form enctype='multipart/form-data' method='POST'>
+        <input type='hidden' name='play_room_id' value='{$gameRoom[$i]['room_id']}' />
+        <input type='hidden' name='play_order' value='{$gameRoom[$i]['room_order']}' />
+        이 파일을 전송합니다: <input name='userfile' type='file' />
+        <input type='submit' name='submit' value='파일 전송' />
+        </form>";
+  }
+  // *** 진행 중인 게임 없을 경우, gameRoom 생성 후 새로고침 ***
+  else {
+    // word 테이블에서 단어 불러오기 후 랜덤으로 한 개 선정
+    $query = "select * from word";
+    $result = $mysqli->query($query);
+    $i = 0;
+    $wordset = array();
+    while($data = mysqli_fetch_array($result)){
+      $wordset[$i]['word_id'] = $data['word_id'];
+      $wordset[$i]['word_data'] = $data['word_data'];
+      $i++;
+    }
+    $answer = mt_rand(0, $i-1);
+
+    // gameRoom 생성 후 새로고침
+    $query = "insert into gameRoom (room_word_id) values ('{$wordset[$answer]['word_id']}')";
+    $mysqli->query($query);
+    echo "<script language='javascript'>
+            document.location.reload();
+          </script>";
+  }
 }
-
 ?>
