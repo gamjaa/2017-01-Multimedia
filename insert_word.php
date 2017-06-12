e7a55389ce8848ff30178ac7997f0ab3e92991b5
WonHo Bark <wonho.park83@gmail.com>
2017-06-12 20:27:45 +0900

    insert_word.php

--- insert_word.php
+++ insert_word.php
@@ -6,20 +6,47 @@
 @require('conn.php');
 global $mysqli;
 
+$query = "SELECT * FROM word WHERE word_data = '".$_POST['word']."';";
+$result=$mysqli->query($query);
 
-$query = "INSERT INTO word (word_data)
-          VALUES ('".$_POST['word']."');";
+if ($result->num_rows > 0) {
+  $i = 0;
+  $arr = array();
+  while($data = mysqli_fetch_array($result)){
+    $arr[$i]['word_id'] = $data['word_id'];
+    $arr[$i]['word_data'] = $data['word_data'];
+    $i++;
+  }
+  echo "word:";
+  echo $_POST['word'];
+  echo "<br>query:";
+  echo $query;
+  echo "<br>result:";
 
-echo "word:";
-echo $_POST['word'];
-echo "<br>query:";
-echo $query;
-echo "<br>result:";
-$result = $mysqli->query($query);
-echo $result;
-echo "<br>num_rows:";
-echo $result->num_rows;
+  for ($i=0; $i < $result->num_rows; $i++) {
+    echo "[".$i."]:(".$arr[$i]['word_id'].") ".$arr[$i]['word_data'];
+  }
+
+  echo "<br>num_rows:";
+  echo $result->num_rows;
+  echo "이미 등록된 단어입니다.";
+
+} else {
+  $query = "INSERT INTO word (word_data)
+            VALUES ('".$_POST['word']."');";
+
+  echo "word:";
+  echo $_POST['word'];
+  echo "<br>query:";
+  echo $query;
+  echo "<br>result:";
+  $result = $mysqli->query($query);
+  echo $result;
+  echo "<br>num_rows:";
+  echo $result->num_rows;
+
+  echo "단어가 추가되었습니다.";
+}
 ?>
-
 </body>
 </html>
