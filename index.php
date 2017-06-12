32acdb72d1db6b611a578aff6e06439689c6b7b0
정경원 <potato93@daum.net>
2017-06-12 19:45:34 +0900

    로그인/로그아웃 링크 추가(오타 수정)

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <a href="draw.php">그림 그리기</a><br>
 <a href="write.php">단어 맞추기</a><br>
 <a href="#">단어 추가</a><br>
-<?php if(isset($_SESSION['user_id']) {
+<?php if(isset($_SESSION['user_id'])) {
   echo "<a href='logout.php'>로그아웃</a>";
 } else {
   echo "<a href='login.php'>로그인</a>";
